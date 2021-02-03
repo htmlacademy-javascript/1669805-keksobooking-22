a2a21b66a27633ed30604454dae4177558d49fc8
Isyan <isyan@mail.ru>
2021-02-03 21:48:40 +0300

    Подключил скрипты к html файлу. Создал две функции возврата случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно. Источник: https://www.turbopro.ru/index.php/yazyk-programmirovaniya-java/7685-8-generatsiya-sluchajnykh-chisel-v-zadannom-diapazone
-let getRandomInteger = function(min, max) {
-  let number = min + Math.random() * (max - min + 1);
-  //  Условие, если максимальное число окажется меньше минимального
-  if (max < min) {
-    alert(number = 'Неправильно заданы аргументы ' + min + ' не может быть меньше, чем ' + max)
+/* Функция, возвращающая случайное целое число из переданного диапазона включительно.
+Источник: https://www.turbopro.ru/index.php/yazyk-programmirovaniya-java/7685-8-generatsiya-sluchajnykh-chisel-v-zadannom-diapazone */
+
+const getRandomInRange = (min, max) => {
+  if (min > max) {
+    throw new Error(`Неправильно заданы аргументы  ${min}  не может быть больше, чем ${max}`)
   }
-  return Math.floor(number);
+  return Math.floor(Math.random() * (max - min) + min);
 }
-let number = getRandomInteger(3, 9);
+getRandomInRange(4, 9);
 
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Источник: https://learn.javascript.ru/number
-let getRandomDotInteger = function(min, max ) {
-  let numberDot = min + Math.random() * (max - min + 1);
-  //  Условие, если максимальное число окажется меньше минимального
-  if (max < min) {
-    alert(numberDot = 'Неправильно заданы аргументы ' + min + ' не может быть меньше, чем ' + max)
+
+/* Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+Источник: https://learn.javascript.ru/number */
+
+const getRandomFloat = (min, max) => {
+  if (min > max) {
+    throw new Error(`Неправильно заданы аргументы  ${min}  не может быть больше, чем ${max}`)
   }
-  return numberDot.toFixed(2);
+  return (Math.random() * (max - min) + min).toFixed(2);
 }
-let numberDot = getRandomDotInteger(3, 6);
-
+alert(getRandomFloat(3, 4));
