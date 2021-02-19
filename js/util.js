48ce9a0c90c8e65e47793fc25d42d80b41ece131
Isyan <isyan@mail.ru>
2021-02-19 22:39:20 +0300

    Выполнил домашку 5.1

--- js/util.js
+++ js/util.js
@@ -37,6 +37,14 @@ const makeUniqueRandomIntegerGenerator = (min, max) => {
     return currentValue;
   };
 };
+// функция склонения окончаний
+const declOfNum = (n, text_forms)=> {
+  n = Math.abs(n) % 100; const n1 = n % 10;
+  if (n > 10 && n < 20) { return text_forms[2]; }
+  if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+  if (n1 == 1) { return text_forms[0]; }
+  return text_forms[2];
+}
 
-export {getRandomFloat, getRandomInteger, addLeadingZero, getRandomItem, makeUniqueRandomIntegerGenerator};
+export {getRandomFloat, getRandomInteger, addLeadingZero, getRandomItem, makeUniqueRandomIntegerGenerator, declOfNum};
 
